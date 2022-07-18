94b93ff983a735dfbfedb2ace288abd3448fec5d
daniyalhotelkey <daniyal.mateen@hotelkeyapp.com>
2022-07-18 12:49:20 +0500

    Final Commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+
+       /*     Using Simple Statements   */
+//        DB_Statements db_statements=new DB_Statements();
+//        db_statements.getData();
+//        db_statements.insertData();
+//        db_statements.updateData();
+//        db_statements.deleteData();
+
+
+        /*     Using Prepared Statements   */
+//        DB_PreparedStatements db_preparedStatements=new DB_PreparedStatements();
+//        db_preparedStatements.insertData();
+//        db_preparedStatements.updateData();
+//        db_preparedStatements.deleteData();
+
+
+        /*     Exploring Transactions   */
+//        DB_Transactions db=new DB_Transactions();
+//        db.addReservation();
+//        db.addGuests();
+
+    }
+}
